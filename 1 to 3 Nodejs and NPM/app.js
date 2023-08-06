8558b12bbd3ecd97e5e5d9618c98918a1d0e8034
Harsh <hgaur491@gmail.com>
2023-08-06 19:13:55 +0530

    completing chapter 1 to 3

--- 1 to 3 Nodejs and NPM/app.js
+++ 1 to 3 Nodejs and NPM/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const http = require("http");
 const { json } = require("stream/consumers");
 const url = require("url")
+const slugify = require("slugify")
 
 const replaceTemplate = require("./modules/replaceTemplate")
 
@@ -31,6 +32,8 @@ const product = fs.readFileSync(`${__dirname}/template/product.html`,"utf-8")
 const data = fs.readFileSync(`${__dirname}/product/product.json`,"utf-8")
 const dataObject=JSON.parse(data)
 
+const slugs = dataObject.map(e=>slugify(e.productName,{lower:true}))
+console.log(slugs)
 
 const server = http.createServer((req,res)=>{
     const {query,pathname} = url.parse(req.url,true);
